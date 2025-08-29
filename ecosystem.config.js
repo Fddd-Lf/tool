d85eadfc01c9258ec20f21671b8d44af71df289b
Foden2 <450868886@qq.com>
2025-08-29 16:54:20 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "next-app", // PM2 进程名
+      name: "nextTool", // PM2 进程名
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000", // Next.js 启动命令
       cwd: "/var/www/tool", // 服务器目录
